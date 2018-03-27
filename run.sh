c8d3ceb61d4fecb8039cb8abd4e8ae54f7582581
Andrey <OxCom@users.noreply.github.com>
2018-03-27 16:49:27 +0200

    Update run.sh
    Update install script to handle host and then fetch content.

--- run.sh
+++ run.sh
@@ -16,17 +16,17 @@ echo "[APT]: Puppet"
 export PATH=/opt/puppetlabs/bin:$PATH
 echo "Puppet version is $(puppet --version)"
 
-echo "[PUPPET]: Control Repo"
-git clone https://github.com/OxCom/puppet-php-skeleton-dev.git
-cp -rf ./puppet-php-skeleton-dev/* /etc/puppetlabs/puppet/
-rm -rf ./puppet-php-skeleton-dev
-
 echo "[SSH]: ===="
 echo "[SSH]: Hosts"
 ssh-keygen -R bitbucket.org
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
 ssh-keygen -R github.com
 ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+echo "[PUPPET]: Control Repo"
+git clone https://github.com/OxCom/puppet-php-skeleton-dev.git
+cp -rf ./puppet-php-skeleton-dev/* /etc/puppetlabs/puppet/
+rm -rf ./puppet-php-skeleton-dev
 
 echo "[PUPPET]: ===="
 echo "[PUPPET]: Running R10K"
