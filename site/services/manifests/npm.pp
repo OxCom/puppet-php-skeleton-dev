0bbe581ff633970a3c529e3d125aaa4a32853e3d
OxCom <gm.oxcom@gmail.com>
2020-03-17 11:44:40 +0100

    WIP: new env

--- site/services/manifests/npm.pp
+++ site/services/manifests/npm.pp
@@ -2,8 +2,6 @@ class services::npm {
     info("Initialize")
 
     class { '::nodejs':
-        manage_package_repo       => true,
-        nodejs_dev_package_ensure => 'present',
-        npm_package_ensure        => 'present',
+        npm_package_ensure => 'present',
     }
 }
