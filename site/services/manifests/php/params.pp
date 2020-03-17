0781812d6e86b6050d426546f9d22d3ebf0da3e1
OxCom <gm.oxcom@gmail.com>
2020-03-17 12:26:41 +0100

    WIP: new env

--- site/services/manifests/php/params.pp
+++ site/services/manifests/php/params.pp
@@ -1,9 +1,9 @@
 class services::php::params {
-  $versions = lookup('php.versions', Array, 'first', [])
-  $packages = lookup('php.packages', Array, 'deep', [])
-  $pools    = lookup('php.packages', Hash, 'first', {})
-  $composer = lookup('composer', Boolean, 'first', false)
+    $versions = lookup('php.versions', Array, 'first', [])
+    $packages = lookup('php.packages', Array, 'deep', [])
+    $pools = lookup('php.pools', Hash, 'first', {})
+    $composer = lookup('composer', Boolean, 'first', false)
 
-  # List of packages without version prefix
-  $common   = ['codesniffer', 'codecoverage', 'xdebug']
+    # List of packages without version prefix
+    $common = ['codesniffer', 'codecoverage', 'xdebug']
 }
