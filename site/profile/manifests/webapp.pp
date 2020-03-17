8371a0141269fdd00e5b75ab7718a0cc9e2f535d
OxCom <gm.oxcom@gmail.com>
2020-03-17 16:04:36 +0100

    WIP: new env

--- site/profile/manifests/webapp.pp
+++ site/profile/manifests/webapp.pp
@@ -10,17 +10,5 @@ class profile::webapp {
   include services::nginx::params
   include services::nginx
 
-  # $versions = lookup('php.versions', Array, 'first', ['php7.0'])
-  # $packages = lookup('php.packages', Array, 'deep', ['php7.0'])
-  # $projects = lookup('projects', Hash, 'first', {})
-  # $composer = lookup('composer', Boolean, 'first', true)
-  # $domain   = lookup('nginx.domain', String, 'first', $::domain)
-  #
-  # class { 'services::nginx':
-  #   versions => $versions,
-  #   projects => $projects,
-  #   domain   => $domain
-  # }
-
-  # Class['services::php'] -> Class['services::nginx']
+  Class['services::php'] -> Class['services::nginx']
 }
