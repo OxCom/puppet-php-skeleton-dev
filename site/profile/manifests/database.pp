f087276e7a43364f60199289adb346d8bd946c62
OxCom <gm.oxcom@gmail.com>
2020-03-17 09:44:07 +0100

    WIP: new env

--- site/profile/manifests/database.pp
+++ site/profile/manifests/database.pp
@@ -1,4 +1,4 @@
 class profile::database {
-  # we can extend it with variables + if/else from hierra
-  # include services::mysql
+  # We can extend it with variables + if/else from hierra
+  include services::mysql
 }
