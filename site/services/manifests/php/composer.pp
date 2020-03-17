40d8e7af825376dc923fb70734c61cb4905c3f0d
OxCom <gm.oxcom@gmail.com>
2020-03-17 14:39:42 +0100

    WIP: new env

--- site/services/manifests/php/composer.pp
+++ site/services/manifests/php/composer.pp
@@ -1,36 +1,35 @@
 class services::php::composer {
     info("Initialize")
 
-    # $web_path = 'https://getcomposer.org/download/${version}/composer.phar'
-    # $install_path = '/usr/local/bin'
-    # $composer_path = "$install_path/composer"
-    # $owner = 'root'
-    #
-    # # TODO: parameters
-    # exec { 'composer-install':
-    #     command     => "/usr/bin/wget --no-check-certificate -O $composer_path $web_path",
-    #     environment => [ "COMPOSER_HOME=$install_path" ],
-    #     user        => $owner,
-    #     unless      => "/usr/bin/test -f ${composer_full_path} && ${composer_full_path} -V |grep -q ${version}",
-    #     timeout     => $download_timeout,
-    #     require     => Package['wget'],
-    # }
-    #
-    # file { "${target_dir}/${command_name}":
-    #     ensure  => file,
-    #     owner   => $user,
-    #     mode    => '0755',
-    #     group   => $group,
-    #     require => Exec['composer-install'],
-    # }
-    #
-    # $ensure = $auto_update ? { true => present, false => absent }
-    # cron { 'composer-update':
-    #     ensure  => $ensure,
-    #     command => "COMPOSER_HOME=${target_dir} ${composer_full_path} self-update -q",
-    #     hour    => 0,
-    #     minute  => fqdn_rand(60),
-    #     user    => $user,
-    #     require => File[$composer_full_path],
-    # }
+    # TODO: parameters
+    $web_path = 'https://getcomposer.org/download/${version}/composer.phar'
+    $install_path = '/usr/local/bin'
+    $composer_path = "$install_path/composer"
+    $owner = 'root'
+    $group = 'root'
+
+    exec { 'composer-install':
+        command     => "/usr/bin/wget --no-check-certificate -O $composer_path $web_path",
+        environment => [ "COMPOSER_HOME=$install_path" ],
+        user        => $owner,
+        unless      => "/usr/bin/test -f $composer_path && $composer_path -V",
+        require     => Package['wget'],
+    }
+
+    file { "$composer_path":
+        ensure  => file,
+        owner   => $owner,
+        mode    => '0755',
+        group   => $group,
+        require => Exec['composer-install'],
+    }
+
+    cron { 'composer-update':
+        ensure  => 'present',
+        command => "COMPOSER_HOME=$install_path $composer_path self-update -q",
+        hour    => 0,
+        minute  => fqdn_rand(60),
+        user    => $owner,
+        require => File[$composer_path],
+    }
 }
