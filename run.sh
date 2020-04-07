d0044cc3ab413ea54f168d96f9762b7a5cae112d
OxCom <gm.oxcom@gmail.com>
2020-04-07 11:46:10 +0200

    update script

--- run.sh
+++ run.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-echo "Initialize"
-# https://docs.puppet.com/puppet/5.1/install_linux.html
-# https://docs.puppet.com/puppet/5.1/puppet_platform.html
-wget --no-verbose https://apt.puppetlabs.com/puppet6-release-xenial.deb
-dpkg -i --force-confdef puppet6-release-xenial.deb
-rm -f puppet6-release-xenial.deb
-
-echo "[APT]: ===="
+echo "[APT]: Update"
 apt-get update
-sudo apt-get upgrade -y
-apt install -y git puppet-agent r10k
-ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+apt-get upgrade -y
 
-echo "[APT]: Puppet"
+echo "[DPKG]: Checking for: puppet6-release"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' puppet6-release|grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+    echo "Installing puppet6-release"
+    wget --no-verbose https://apt.puppetlabs.com/puppet6-release-xenial.deb
+    dpkg -i --force-confdef puppet6-release-xenial.deb
+    rm -f puppet6-release-xenial.deb
+fi
+
+if [ ! -L "/usr/bin/puppet" ] || [ ! -e "/usr/bin/puppet" ]; then
+    ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+fi
+
+echo "Checking for: puppet-agent"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' puppet-agent|grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+    echo "Installing puppet-agent"
+    apt-get install -y puppet-agent
+fi
+
+echo "Checking for: git"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' puppet-agent|grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+    echo "Installing git"
+    apt-get install -y git
+fi
+
+echo "Checking for: r10k"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' puppet-agent|grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+    echo "Installing r10k"
+    apt-get install -y r10k
+fi
+
 export PATH=/opt/puppetlabs/bin:$PATH
-echo "Puppet version is $(puppet --version)"
-
-echo "[SSH]: ===="
-echo "[SSH]: Hosts"
-ssh-keygen -R bitbucket.org
-ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
-ssh-keygen -R github.com
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+echo "[PUPPET]: version is $(puppet --version)"
 
 echo "[PUPPET]: Control Repo"
 git clone https://github.com/OxCom/puppet-php-skeleton-dev.git
 cp -rf ./puppet-php-skeleton-dev/* /etc/puppetlabs/puppet/
 rm -rf ./puppet-php-skeleton-dev
 
-echo "[PUPPET]: ===="
 echo "[PUPPET]: Running R10K"
 cd /etc/puppetlabs/puppet
 r10k deploy environment -p -v
