39487c96bc2d21b215a5a84a958550e7029547f6
OxCom <gm.oxcom@gmail.com>
2020-04-07 16:19:39 +0200

    php: composer

--- site/lib/facts.d/user_home_dirs.rb
+++ site/lib/facts.d/user_home_dirs.rb
@@ -2,6 +2,9 @@
 
 Facter.add("user_home_dirs") do
   setcode do
+    value = []
     value = Dir.entries(root).select { |entry| File.directory? File.join(root, entry) and not entry.in? %w[. ..]}
+
+    value
   end
 end
