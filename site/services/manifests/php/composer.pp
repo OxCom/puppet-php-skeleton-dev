7930008f15ae7bd216df41b81aa4c7b91ed1c34b
OxCom <gm.oxcom@gmail.com>
2020-04-07 12:16:49 +0200

    php: packages

--- site/services/manifests/php/composer.pp
+++ site/services/manifests/php/composer.pp
@@ -37,10 +37,10 @@ class services::php::composer {
 
     info("Install global plugin: hirak/prestissimo")
     exec { 'composer-parallel':
-        command     => 'composer global remove hirak/prestissimo',
+        command     => "COMPOSER_HOME=$install_path $composer_path global remove hirak/prestissimo",
         environment => [ "COMPOSER_HOME=$install_path" ],
         user        => $owner,
-        unless      => "$composer_path global show -n 2>&1 | grep hirak/prestissimo",
+        unless      => "COMPOSER_HOME=$install_path $composer_path global show -n 2>&1 | grep hirak/prestissimo",
         require => Cron['composer-update'],
     }
 }
