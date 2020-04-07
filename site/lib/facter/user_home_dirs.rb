9fad860999637e664e7f5980135140de69d26a2f
OxCom <gm.oxcom@gmail.com>
2020-04-07 16:28:00 +0200

    custom facts

--- site/lib/facter/user_home_dirs.rb
+++ site/lib/facter/user_home_dirs.rb
@@ -1,10 +1,6 @@
-# user_home_dirs.rb
-
-Facter.add("user_home_dirs") do
+Facter.add(":user_home_dirs") do
   setcode do
-    value = []
     value = Dir.entries(root).select { |entry| File.directory? File.join(root, entry) and not entry.in? %w[. ..]}
-
     value
   end
 end
