d868a0d7277a9d70357be567aa46224aa7854659
OxCom <gm.oxcom@gmail.com>
2020-06-18 22:39:27 +0200

    fix: imagick slow with native lib

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -5,10 +5,24 @@ class profile::default {
     # the base profile should include component modules that will be on all nodes
     $tools = lookup('tools', Array, 'deep', []);
     $tools.each |Integer $index, String $tool| {
+
         package { $tool:
             ensure => present
+        }
+
+        if ($tool == 'imagemagick') {
+            info("Fix: slow webp convert in Imagick and Webp.")
+            tidy { 'webp-fix':
+                path    => '/usr/lib/',
+                recurse => true,
+                age => 0,
+                matches => ['webp.so', 'webp.la'],
+                require => Package[$tool]
+            }
         }
     }
 
     include services::openssl
+
+
 }
