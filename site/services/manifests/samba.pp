c7bee578203b378294bf42a66d3e427506d21544
OxCom <gm.oxcom@gmail.com>
2020-09-24 07:43:11 +0200

    smb

--- site/services/manifests/samba.pp
+++ site/services/manifests/samba.pp
@@ -1,9 +1,33 @@
 class services::samba (
-    String $versions = $services::samba::workgroup,
+    String $workgroup = $services::samba::workgroup,
+    String $allow_guests = $services::samba::allow_guests,
 ) inherits services::samba::params {
     info("Initialize")
 
     package { ['samba']:
         ensure => present,
+    }
+
+    service { "samba":
+        ensure  => 'running',
+        enable  => 'true',
+        require => Package['samba'],
+    }
+
+    $shared = [
+        {
+            name => 'storage',
+            path => '/var/storage',
+            writable => 'yes',
+        }
+    ];
+
+    file { '/etc/samba/smb.conf':
+        ensure  => file,
+        content => template('services/samba/smb.conf.erb'),
+        notify  => Service['samba'],
+        require => [
+            Package['samba']
+        ]
     }
 }
--- site/services/manifests/samba/params.pp
+++ site/services/manifests/samba/params.pp
@@ -1,3 +1,4 @@
 class services::samba::params {
     $workgroup = lookup('samba.workgroup', String, 'first', 'WORKGROUP')
+    $allow_guests = lookup('samba.allow_guests', String, 'first', 'yes')
 }
