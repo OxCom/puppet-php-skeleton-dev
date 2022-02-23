7c396d487194ea3a9a923ad7247116e34ac3749f
Andrey <OxCom@users.noreply.github.com>
2022-02-23 11:29:08 +0100

    Update www.pp

--- site/services/manifests/nginx/www.pp
+++ site/services/manifests/nginx/www.pp
@@ -149,7 +149,7 @@ class services::nginx::www (
             state        => 'Localhost',
             locality     => 'VM',
             unit         => 'Developer instance',
-            altnames     => ["$project.$domain", "www.$domain", "$domain"],
+            altnames     => ["*.$project.$domain", "$project.$domain", "www.$domain", "$domain"],
             email        => "admin@$project.$domain",
             days         => 3650,
             base_dir     => '/etc/nginx/ssl',
