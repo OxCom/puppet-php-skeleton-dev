4c8fab5715042359c8a3d928c4433d972241b7f8
Andrii Afanasiev <gm.oxcom@gmail.com>
2024-02-29 11:23:11 +0100

    fix: php pool configs with not php

--- site/services/manifests/php/pool.pp
+++ site/services/manifests/php/pool.pp
@@ -38,7 +38,7 @@ class services::php::pool (
             }
 
             $list.each |Integer $index, Hash $pool| {
-                if !defined($pool['php']) {
+                if !$pool['php'] {
                     next()
                 }
 
