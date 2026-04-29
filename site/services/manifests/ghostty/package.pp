68bd3f901f1e185a041d8a322c83ac60e463f890
Andrii Afanasiev <gm.oxcom@gmail.com>
2026-04-29 09:52:09 +0200

    switch wezterm to ghostty

--- site/services/manifests/ghostty/package.pp
+++ site/services/manifests/ghostty/package.pp
@@ -1,14 +1,21 @@
 class services::ghostty::package {
   info("Initialize")
 
-  package { 'ghostty':
+  package { 'snapd':
     ensure => present,
   }
 
+  exec { 'install-ghostty-snap':
+    command => '/usr/bin/snap install ghostty',
+    path    => '/bin:/usr/bin:/snap/bin',
+    unless  => '/bin/bash -o pipefail -c "snap list ghostty >/dev/null 2>&1"',
+    require => Package['snapd'],
+  }
+
   exec { 'set-default-terminal-ghostty':
-    command => '/bin/bash -o pipefail -c "update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/ghostty 70 && update-alternatives --set x-terminal-emulator /usr/bin/ghostty"',
+    command => '/bin/bash -o pipefail -c "update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /snap/bin/ghostty 70 && update-alternatives --set x-terminal-emulator /snap/bin/ghostty"',
     path    => '/bin:/usr/bin',
-    unless  => '/bin/bash -o pipefail -c \'test "$(readlink -f /etc/alternatives/x-terminal-emulator 2>/dev/null || true)" = "/usr/bin/ghostty"\'',
-    require => Package['ghostty'],
+    unless  => '/bin/bash -o pipefail -c \'test "$(readlink -f /etc/alternatives/x-terminal-emulator 2>/dev/null || true)" = "/snap/bin/ghostty"\'',
+    require => Exec['install-ghostty-snap'],
   }
 }
