823ff1691f14023d255fc767a0b13bcfdf136f83
Andrii Afanasiev <gm.oxcom@gmail.com>
2026-04-29 10:14:18 +0200

    update issue with key bindings

--- site/services/manifests/ghostty/package.pp
+++ site/services/manifests/ghostty/package.pp
@@ -18,4 +18,24 @@ class services::ghostty::package {
     unless  => '/bin/bash -o pipefail -c \'test "$(readlink -f /etc/alternatives/x-terminal-emulator 2>/dev/null || true)" = "/snap/bin/ghostty"\'',
     require => Exec['install-ghostty-snap'],
   }
+
+  $ghostty_keybinding_path    = '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ghostty/'
+  $ghostty_custom_keybindings = "['${ghostty_keybinding_path}']"
+  $ghostty_media_keys_schema  = 'org.gnome.settings-daemon.plugins.media-keys'
+  $ghostty_binding_schema     = "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${ghostty_keybinding_path}"
+  $gsettings_available_check  = '/bin/bash -o pipefail -c "command -v gsettings >/dev/null 2>&1"'
+
+  if $facts['user_home_accounts'].is_a(Array) and !empty($facts['user_home_accounts']) {
+    $facts['user_home_accounts'].each |Hash $account| {
+      $desktop_user = $account['user']
+
+      exec { "set-ghostty-media-keybinding-${desktop_user}":
+        command => "/bin/bash -o pipefail -c \"runuser -u ${desktop_user} -- dbus-run-session -- gsettings set ${ghostty_media_keys_schema} terminal '@as []' && runuser -u ${desktop_user} -- dbus-run-session -- gsettings reset ${ghostty_media_keys_schema} custom-keybindings && runuser -u ${desktop_user} -- dbus-run-session -- gsettings set ${ghostty_media_keys_schema} custom-keybindings \\\"${ghostty_custom_keybindings}\\\" && runuser -u ${desktop_user} -- dbus-run-session -- gsettings set ${ghostty_binding_schema} name 'Ghostty' && runuser -u ${desktop_user} -- dbus-run-session -- gsettings set ${ghostty_binding_schema} command '/snap/bin/ghostty' && runuser -u ${desktop_user} -- dbus-run-session -- gsettings set ${ghostty_binding_schema} binding '<Control><Alt>t'\"",
+        unless  => "/bin/bash -o pipefail -c \"runuser -u ${desktop_user} -- dbus-run-session -- gsettings get ${ghostty_media_keys_schema} terminal | grep -Fx '@as []' >/dev/null && runuser -u ${desktop_user} -- dbus-run-session -- gsettings get ${ghostty_media_keys_schema} custom-keybindings | grep -Fx \\\"${ghostty_custom_keybindings}\\\" >/dev/null && runuser -u ${desktop_user} -- dbus-run-session -- gsettings get ${ghostty_binding_schema} name | grep -Fx \\\"'Ghostty'\\\" >/dev/null && runuser -u ${desktop_user} -- dbus-run-session -- gsettings get ${ghostty_binding_schema} command | grep -Fx \\\"'/snap/bin/ghostty'\\\" >/dev/null && runuser -u ${desktop_user} -- dbus-run-session -- gsettings get ${ghostty_binding_schema} binding | grep -Fx \\\"'<Control><Alt>t'\\\" >/dev/null\"",
+        onlyif  => $gsettings_available_check,
+        path    => '/bin:/usr/bin',
+        require => Exec['set-default-terminal-ghostty'],
+      }
+    }
+  }
 }
